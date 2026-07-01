460bfb7a8c8ae0e00cf972e64035d585429f7bc6
umesbrejoes <umesbrejoes@gmail.com>
2026-07-01 20:14:49 -0300

    Add Firebase configuration and initialization
    This file centralizes the Firebase configuration and initialization for the election system project.

---
+++ js/js/firebase.js
@@ -0,0 +1,65 @@
+// ======================================================
+// SISTEMA OFICIAL DAS ELEIÇÕES
+// UMES BREJÕES 2026
+//
+// Arquivo: firebase.js
+// Centraliza toda a configuração do Firebase.
+// ======================================================
+
+// =========================
+// IMPORTS
+// =========================
+
+import { initializeApp }
+
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import {
+
+getFirestore,
+
+serverTimestamp
+
+}
+
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+// =========================
+// CONFIGURAÇÃO
+// =========================
+
+const firebaseConfig = {
+
+apiKey:"AIzaSyBCgk_6cEcCF4_O2pACTSkhk1IInC-1Uro",
+authDomain:"umes-brejoes-eleicoes-2026.firebaseapp.com",
+projectId:"umes-brejoes-eleicoes-2026",
+storageBucket:"umes-brejoes-eleicoes-2026.firebasestorage.app",
+messagingSenderId:"406100216755",
+appId:"1:406100216755:web:f994e5e2d386a2e97ed9c6",
+measurementId:"G-F5EWF15PVY"
+
+};
+
+// =========================
+// INICIALIZAÇÃO
+// =========================
+
+const app =
+initializeApp(firebaseConfig);
+
+const db =
+getFirestore(app);
+
+// =========================
+// EXPORTS
+// =========================
+
+export{
+
+app,
+
+db,
+
+serverTimestamp
+
+};
