5f2a5fee4a69d8e54e73cd14aeaa4ad9dd0e2ce6
umesbrejoes <umesbrejoes@gmail.com>
2026-07-01 22:28:24 -0300

    Refactor Firebase configuration and imports

--- js/js/firebase.js
+++ js/js/firebase.js
@@ -3,62 +3,77 @@
 // UMES BREJÕES 2026
 //
 // Arquivo: firebase.js
-// Centraliza toda a configuração do Firebase.
+// Configuração Central do Firebase
 // ======================================================
 
-// =========================
+// ======================================================
 // IMPORTS
-// =========================
+// ======================================================
 
-import { initializeApp }
-
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
 
 getFirestore,
-
 serverTimestamp
 
-}
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import {
 
-// =========================
+getAuth
+
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+// ======================================================
 // CONFIGURAÇÃO
-// =========================
+// ======================================================
 
 const firebaseConfig = {
 
-apiKey:"AIzaSyBCgk_6cEcCF4_O2pACTSkhk1IInC-1Uro",
-authDomain:"umes-brejoes-eleicoes-2026.firebaseapp.com",
-projectId:"umes-brejoes-eleicoes-2026",
-storageBucket:"umes-brejoes-eleicoes-2026.firebasestorage.app",
-messagingSenderId:"406100216755",
-appId:"1:406100216755:web:f994e5e2d386a2e97ed9c6",
-measurementId:"G-F5EWF15PVY"
+apiKey: "AIzaSyBCgk_6cEcCF4_O2pACTSkhk1IInC-1Uro",
+
+authDomain:
+"umes-brejoes-eleicoes-2026.firebaseapp.com",
+
+projectId:
+"umes-brejoes-eleicoes-2026",
+
+storageBucket:
+"umes-brejoes-eleicoes-2026.firebasestorage.app",
+
+messagingSenderId:
+"406100216755",
+
+appId:
+"1:406100216755:web:f994e5e2d386a2e97ed9c6",
+
+measurementId:
+"G-F5EWF15PVY"
 
 };
 
-// =========================
+// ======================================================
 // INICIALIZAÇÃO
-// =========================
+// ======================================================
 
-const app =
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-const db =
-getFirestore(app);
+const db = getFirestore(app);
 
-// =========================
-// EXPORTS
-// =========================
+const auth = getAuth(app);
 
-export{
+// ======================================================
+// EXPORTAÇÕES
+// ======================================================
+
+export {
 
 app,
 
 db,
+
+auth,
 
 serverTimestamp
 
